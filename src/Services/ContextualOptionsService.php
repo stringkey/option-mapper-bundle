5b4fc8c0d5acc0cf2113e44b3403a8b68ab4dd75
peer <stringkey@wxs.nl>
2025-01-09 20:46:39 +0100

    Initial version of service to manage the contextual options

--- src/Services/ContextualOptionsService.php
+++ src/Services/ContextualOptionsService.php
@@ -1,8 +1,44 @@
-<?php
+<?php declare(strict_types=1);
 
 namespace Stringkey\OptionMapperBundle\Services;
 
+use Doctrine\ORM\EntityManagerInterface;
+use Stringkey\OptionMapperBundle\Entity\CustomOption;
+use Stringkey\OptionMapperBundle\Entity\OptionLink;
+use Stringkey\OptionMapperBundle\Exception\UnequalOptionGroupException;
+
 class ContextualOptionsService
 {
+    public function __construct(private readonly EntityManagerInterface $entityManager)
+    {
 
+    }
+
+    public function create(OptionLink $optionLink, bool $flushNow = true): void
+    {
+        $this->entityManager->persist($optionLink);
+        if ($flushNow) {
+            $this->entityManager->flush();
+        }
+    }
+
+    public static function constructOptionLink(
+        CustomOption $sourceOption,
+        CustomOption $targetOption,
+        int $ordinality = 0
+    ): OptionLink {
+
+        // Check if the source and the target group are the same
+        if ($sourceOption->getOptionGroup() !== $targetOption->getOptionGroup()) {
+            throw new UnequalOptionGroupException();
+        }
+
+        $optionLink = new OptionLink();
+
+        $optionLink->setSourceOption($sourceOption);
+        $optionLink->setTargetOption($targetOption);
+        $optionLink->setOrdinality($ordinality);
+
+        return $optionLink;
+    }
 }
